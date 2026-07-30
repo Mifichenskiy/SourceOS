5f3ad78ea4621d418771f32bc6420905240180eb
Mifichenskiy <dimamsm228@gmail.com>
2026-07-30 14:10:58 +0500

    Update template.lua

--- plugins_source/template.lua
+++ plugins_source/template.lua
@@ -1,2 +1,5 @@
---  Just write your plugins like regular code and enter the plugin name without the .lua extension—thanks for listening, the rest is up to you!
+-- Just write your plugins like regular code.
+-- Enter the plugin name without the .lua extension.
+-- Thanks for listening, the rest is up to you!
+
 print("Hello, SourceOS!")
